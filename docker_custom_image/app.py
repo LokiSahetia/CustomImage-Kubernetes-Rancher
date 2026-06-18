467bedcdc858fd8983ebf0230e27d52c6bf41824
Lokesh Sahetia <168418823+LokiSahetia@users.noreply.github.com>
2026-06-19 00:36:25 +0530

    Update app.py

--- docker_custom_image/app.py
+++ docker_custom_image/app.py
@@ -19,7 +19,7 @@ def home():
     conn = get_connection()
     cursor = conn.cursor()
 
-    cursor.execute("SELECT name FROM users")
+    cursor.execute("SELECT * FROM users where name="Loki"")
 
     rows = cursor.fetchall()
 
