abc282a3c4e8885a35a73b9d75ef54f3c2da14c0
Lokesh Sahetia <168418823+LokiSahetia@users.noreply.github.com>
2026-06-19 00:56:57 +0530

    Update app.py

--- docker_custom_image/app.py
+++ docker_custom_image/app.py
@@ -28,7 +28,7 @@ def home():
 
     result = [row[0] for row in rows]
 
-    return f"Showing all the users in database: {', '.join(result)}"
+    return f"Users: {', '.join(result)}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
