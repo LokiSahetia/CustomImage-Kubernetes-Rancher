81372aee1f71191b676de079cf34c7d21bf192f2
Lokesh Sahetia <168418823+LokiSahetia@users.noreply.github.com>
2026-06-18 15:38:57 +0530

    Update app.py

--- docker_custom_image/app.py
+++ docker_custom_image/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Bello World"
+    return "GOOOD MORNING MUMBAI"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
